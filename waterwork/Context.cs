f9703f6d089a8116968314179c8100951d570376
Nirun Tulayote <nirunnu1@hotmail.com>
2017-04-27 18:04:12 +0700

    --

--- waterwork/Context.cs
+++ waterwork/Context.cs
@@ -13,7 +13,6 @@ namespace waterwork
             //Database.Connection.ConnectionString = @"workstation id=waterwork.mssql.somee.com;packet size=4096;user id=nirunnu1_SQLLogin_1;pwd=nbu88xrhaj;data source=waterwork.mssql.somee.com;persist security info=False;initial catalog=waterwork";
            // Database.Connection.ConnectionString = @"Server=127.0.0.1;Database= waterwork ;User Id = sa ;Password=123456;";
             Database.Connection.ConnectionString = @"Server=DESKTOP-HFL5E6G;Database=waterwork;Integrated Security=yes;";
-
         }
         public DbSet<customers> customers { get; set; }
         public DbSet<customer_services> customer_services { get; set; }
@@ -23,7 +22,5 @@ namespace waterwork
         public DbSet<place> place { get; set; }
         public DbSet<amphur> amphur { get; set; }
         public DbSet<province> province { get; set; }
-
-
     }
 }
